12ef33cd9cce2c15f6fbb029657755aa5d8e402e
Mathieu Bridon <bochecha@fedoraproject.org>
2013-04-10 14:07:18 +0800

    templates: Introduce a base layout
    This will be reused for all our views.

--- uptrack/__init__.py
+++ uptrack/__init__.py
@@ -1,8 +1,15 @@
 from pyramid.config import Configurator
+from pyramid.events import BeforeRender
+from pyramid.renderers import get_renderer
 
 from sqlalchemy import engine_from_config
 
 from .models import DBSession, Base
+
+
+def add_base_template(event):
+    layout = get_renderer('templates/layout.pt').implementation()
+    event.update({'layout': layout})
 
 
 def main(global_config, **settings):
@@ -13,6 +20,7 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
 
     config = Configurator(settings=settings)
+    config.add_subscriber(add_base_template, BeforeRender)
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('overview', '/')
