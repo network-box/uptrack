b1a126e5981ce6118373363423f3a3a1c81cac60
Mathieu Bridon <bochecha@fedoraproject.org>
2013-04-10 13:21:12 +0800

    Write imports the way I prefer

--- uptrack/__init__.py
+++ uptrack/__init__.py
@@ -1,10 +1,8 @@
 from pyramid.config import Configurator
+
 from sqlalchemy import engine_from_config
 
-from .models import (
-    DBSession,
-    Base,
-    )
+from .models import DBSession, Base
 
 
 def main(global_config, **settings):
--- uptrack/scripts/initializedb.py
+++ uptrack/scripts/initializedb.py
@@ -4,16 +4,9 @@ import transaction
 
 from sqlalchemy import engine_from_config
 
-from pyramid.paster import (
-    get_appsettings,
-    setup_logging,
-    )
+from pyramid.paster import get_appsettings, setup_logging
 
-from ..models import (
-    DBSession,
-    MyModel,
-    Base,
-    )
+from ..models import DBSession, Base, MyModel
 
 
 def usage(argv):
