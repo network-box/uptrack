ae4225b82c377fb545f759ce6e26cf9c74318cdc
Mathieu Bridon <bochecha@fedoraproject.org>
2013-04-10 15:15:31 +0800

    models: New Release model

--- uptrack/models.py
+++ uptrack/models.py
@@ -1,7 +1,5 @@
-from sqlalchemy import Column, Integer, Text
-
+from sqlalchemy import Column, Integer, Unicode
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -11,12 +9,14 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class Release(Base):
+    __tablename__ = 'release'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+    name = Column(Unicode, unique=True, nullable=False)
+    koji_tag = Column(Unicode, unique=True, nullable=False)
+    git_url = Column(Unicode, unique=True, nullable=False)
 
-    def __init__(self, name, value):
+    def __init__(self, name=None, koji_tag=None, git_url=None):
         self.name = name
-        self.value = value
+        self.koji_tag = koji_tag
+        self.git_url = git_url
