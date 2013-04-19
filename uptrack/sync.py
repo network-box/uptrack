9c56551554809807177ef04f64dc294dc2969892
Mathieu Bridon <bochecha@fedoraproject.org>
2013-04-19 11:46:49 +0800

    sync: Query the latest builds from each release
    This is the first part of the work, and queries Koji to get the
    information.

--- uptrack/sync.py
+++ uptrack/sync.py
@@ -18,9 +18,19 @@
 
 import logging
 
+import koji
+
 import transaction
 
 from uptrack.models import DBSession, Release
+
+
+class Build(object):
+    def __init__(self, name, epoch, version, release):
+        self.name = unicode(name)
+        self.epoch = unicode(epoch) if epoch is not None else u'0'
+        self.version = unicode(version)
+        self.release = unicode(release)
 
 
 class Sync(object):
@@ -28,12 +38,27 @@ class Sync(object):
         self.settings = settings
         self.log = logging.getLogger("uptrack")
 
+    def get_latest_builds(self, tag):
+        """Get the latest builds from Koji
+
+        :param hub_url: The URL of the Koji Hub.
+        :param tag: The stable tag for the distro release.
+        """
+        conn = koji.ClientSession(self.settings["kojihub_url"])
+
+        for build in conn.getLatestBuilds(tag):
+            yield Build(build["name"], build["epoch"], build["version"],
+                        build["release"])
+
     def run(self):
         """Run the sync"""
         releases = DBSession.query(Release)
 
         for release in releases:
             self.log.info("Synchronizing %s..." % release.name)
-            # TODO: Actually synchronize
+            builds = self.get_latest_builds(release.koji_tag)
+
+            # TODO: Update the packages in DB with the build info
+            # TODO: Compare with the latest version from upstream
 
         transaction.commit()
