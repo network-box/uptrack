9b433bb7d3249f50d7f74320f61e74bcd33309d7
Mathieu Bridon <bochecha@fedoraproject.org>
2013-06-21 13:23:37 +0800

    overview: Make sure blocks have the same height in a given row
    Using visible blocks made the overview ugly if each blocks inside a same
    row had different heights.
    
    This fixes that, although not necessarily in a very elegant way.

--- uptrack/static/js/uptrack.js
+++ uptrack/static/js/uptrack.js
@@ -159,3 +159,18 @@ ajaxify_form = function() {
         return false;
     });
 }
+
+equalize_column_heights = function(row_selector, column_selector) {
+    $(row_selector).each(function(index, row) {
+        tallest_height = 0;
+
+        $(row).find(column_selector).each(function(index, block) {
+            current_height = $(block).height();
+            if (current_height >= tallest_height) {
+                tallest_height = current_height;
+            }
+        });
+
+        $(row).find(column_selector).height(tallest_height);
+    });
+}
