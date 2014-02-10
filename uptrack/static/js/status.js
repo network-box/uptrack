5ed99a6a02a771cbbf5e5869e755275e9aaef070
Mathieu Bridon <bochecha@fedoraproject.org>
2014-02-10 16:53:26 +0800

    downstream: Allow unmarking a package as downstream-only

--- uptrack/static/js/status.js
+++ uptrack/static/js/status.js
@@ -15,6 +15,8 @@ var make_popovers = function(popover_class) {
 
             if (action === "downstream") {
                 $(this).click({pkgid: pkgid}, mark_downstream_clicked);
+            } else if (action === "nodownstream") {
+                $(this).click({pkgid: pkgid}, unmark_downstream_clicked);
             } else {
                 console.log("ERROR: Can't handle '" + action + "' action");
             }
@@ -25,6 +27,25 @@ var make_popovers = function(popover_class) {
 var mark_downstream_clicked = function(e) {
     var pkgid = e.data.pkgid;
     var url = "/packages/" + pkgid + "/markdownstream";
+
+    $("#popover_button_" + pkgid).popover("hide");
+
+    $.get(url, {}, function(data) {
+        var notif = $("#alert_placeholder");
+
+        if (data.error !== undefined) {
+            $(notif).html('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><span>' + data.error + '</span></div>');
+        } else {
+            $(notif).html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>' + data.msg + '</span></div>');
+        }
+    }, 'json');
+
+    return false;
+}
+
+var unmark_downstream_clicked = function(e) {
+    var pkgid = e.data.pkgid;
+    var url = "/packages/" + pkgid + "/unmarkdownstream";
 
     $("#popover_button_" + pkgid).popover("hide");
 
